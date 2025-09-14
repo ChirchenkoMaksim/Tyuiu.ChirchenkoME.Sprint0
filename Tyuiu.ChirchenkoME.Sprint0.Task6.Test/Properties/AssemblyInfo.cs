57d263eb116bc6808e9d953a09492b41bfebf616
Administrator <Administrator@WIN-M6NM0GBOMH4>
2025-09-14 18:56:17 +0500

    Task6

---
+++ Tyuiu.ChirchenkoME.Sprint0.Task6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChirchenkoME.Sprint0.Task6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChirchenkoME.Sprint0.Task6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10f534bb-ccac-40dc-8c6d-581a78cb5094")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
